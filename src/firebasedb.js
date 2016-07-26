661d1f5a2337355225e2c5a419bbd8c93f573f40
Ian Bateman <ian.h.bateman.18@dartmouth.edu>
2016-07-26 14:10:13 -0400

    added more comments

--- src/firebasedb.js
+++ src/firebasedb.js
@@ -10,24 +10,30 @@ firebase.initializeApp(config);
 
 const database = firebase.database().ref('notes');
 // mostly given
+// some snapshot usage found https://www.firebase.com/docs/web/api/datasnapshot/
+// and at https://www.firebase.com/docs/web/api/datasnapshot/val.html
 export function fetchNotes(callback) {
   firebase.database().ref('notes').on('value', (snapshot) => {
     callback(snapshot.val());
   });
 }
 // function to update noteObj location in firebase DB
+// mostly given, update usage found https://www.firebase.com/docs/web/api/firebase/update.html
 export function moveIdDB(id, x, y) {
   database.child(id).update({ x, y });
 }
 // function to update noteObj text in firebase DB
+// mostly given, update usage found https://www.firebase.com/docs/web/api/firebase/update.html
 export function updateIdDB(id, text) {
   database.child(id).update({ text });
 }
 // function to delete noteObj from firebase DB
+// given
 export function deleteIdDB(id) {
   database.child(id).remove();
 }
 // function to add noteObj to firebase DB
+// mostly given, push usage found https://www.firebase.com/docs/web/api/firebase/push.html
 export function addNoteDB(note) {
   database.push(note);
 }
